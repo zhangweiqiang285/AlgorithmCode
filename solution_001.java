aff451911859f385e1ad6f264f25b3795d91eed4
zhangweiqiang285 <zhangweiqiang285@yeah.net>
2019-01-02 23:45:07 +0800

    Create solution_001.java

---
+++ solution_001.java
@@ -0,0 +1,23 @@
+package Code;
+
+import java.util.*;
+
+public class solution_001 {
+	
+	public int[] TwoSum(int[] nums, int target)
+	{
+		Map<Integer,Integer> hmp = new HashMap<>();
+		int len = nums.length;
+		for(int i = 0; i< len; i++)
+		{
+			
+		}
+		//return {};
+	}
+	
+	
+	public static void main(String[] main)
+	{
+		
+	}
+}
